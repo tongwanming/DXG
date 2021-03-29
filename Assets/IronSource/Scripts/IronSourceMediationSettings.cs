44bf070efba7d3f85fe107ff69d4c3058002c28f
naruto <naruto@newborntown.com>
2021-03-29 14:18:12 +0800

    Feature: 更新IronSource广告库

---
+++ Assets/IronSource/Scripts/IronSourceMediationSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class IronSourceMediationSettings : ScriptableObject
+{
+	public static readonly string IRONSOURCE_SETTINGS_ASSET_PATH = Path.Combine(IronSourceConstants.IRONSOURCE_RESOURCES_PATH, IronSourceConstants.IRONSOURCE_MEDIATION_SETTING_NAME + ".asset");
+
+	[Header("Ironsource AppKey")]
+	[Tooltip("Add your application AppKeys, as provided in Ironsource Platform")]
+	public string AndroidAppKey = string.Empty;
+	[Tooltip("Add your application AppKeys, as provided in Ironsource Platform")]
+	public string IOSAppKey = string.Empty;
+
+	[Header("Automatic Initialization")]
+	[Tooltip("Use this flag when you wish to initialize all ad units (recommended)")]
+	public bool EnableIronsourceSDKInitAPI;
+
+    [Header("Ironsource SKAdNetwork ID")]
+	[Tooltip("Add Ironsource SKAdNetworkIdentifier to your Info.plist for iOS 14+")]
+    public bool AddIronsourceSkadnetworkID;
+
+    [Header("Project Features")]
+	public bool EnableAdapterDebug;
+
+	public bool EnableIntegrationHelper;
+}
